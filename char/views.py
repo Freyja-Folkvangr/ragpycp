1d5ea4eecd24a890fd9be629ebbc0b3d360eb540
Giuliano P. Tognarelli Buono-core <gtognarelli@linux.com>
2019-05-24 15:04:45 -0400

    Support to reset char position, look and save point

--- char/views.py
+++ char/views.py
@@ -10,8 +10,50 @@ def char(request):
         'chars': chars,
         'char_count': len(chars)
     }
-    print(context)
     return render(request, 'char.html', context)
+
+
+def char_reset_position(request, char_id):
+    if request.POST:
+        if 'reset-position' in request.POST:
+            pass
+        elif 'reset-map' in request.POST:
+            return char_reset_map(request, char_id)
+        elif 'reset-appearance' in request.POST:
+            return char_reset_appearence(request, char_id)
+
+        char = Char.objects.get(char_id=char_id)
+
+        if char.account_id.id != request.user.id:
+            return redirect('forbidden')
+        char.reset_position()
+        return redirect('char:char_view', char_id)
+    else:
+        return redirect('not_found')
+
+
+def char_reset_map(request, char_id):
+    if request.POST:
+        char = Char.objects.get(char_id=char_id)
+
+        if char.account_id.id != request.user.id:
+            return redirect('forbidden')
+        char.reset_save_position()
+        return redirect('char:char_view', char_id)
+    else:
+        return redirect('not_found')
+
+
+def char_reset_appearence(request, char_id):
+    if request.POST:
+        char = Char.objects.get(char_id=char_id)
+
+        if char.account_id.id != request.user.id:
+            return redirect('forbidden')
+        char.reset_appearence()
+        return redirect('char:char_view', char_id)
+    else:
+        return redirect('not_found')
 
 
 def char_view(request, char_id):
