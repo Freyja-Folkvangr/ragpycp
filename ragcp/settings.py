11924dbc6ce40060d474262d42b51089f3dd149e
Giuliano P. Tognarelli Buono-core <gtognarelli@linux.com>
2020-05-10 14:13:11 -0400

    test

--- ragcp/settings.py
+++ ragcp/settings.py
@@ -39,7 +39,7 @@ if host:
     if host in ['127.0.0.1', 'localhost'] or '.local' in host:
         debug = True
     else:
-        DEBUG = False
+        DEBUG = True
 else:
     DEBUG = True
 logger.info('DEBUG %s' % DEBUG)
