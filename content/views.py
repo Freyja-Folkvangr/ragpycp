93a6bec3d79681424290601a5c39cdfecfb31103
Giuliano P. Tognarelli Buono-core <gtognarelli@linux.com>
2020-10-06 18:06:52 -0300

    Multiline support for posts

--- content/views.py
+++ content/views.py
@@ -20,6 +20,7 @@ def get_feed(request):
     posts = Post.objects.filter(author__sex='S', parent=None).values_list('title', 'content')
     logger.debug('Existing posts: %s' % posts)
     for post in feed['entries']:
+        post.description = post.description.replace('\n', '<br>')
         if (post.title, post.description) not in posts:
             p = Post(
                 title=post.title,
@@ -39,6 +40,7 @@ def new_entry(request):
     if form.is_valid():
         post = form.save(commit=False)
         post.author = request.user
+        post.content = post.content.replace('\n', '<br>')
         form.save()
         return redirect('index')
 
