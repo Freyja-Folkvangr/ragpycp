8d3e84ac89f3115a769c8fd54ea95ac9792bf918
Giuliano P. Tognarelli Buono-core <gtognarelli@linux.com>
2020-10-06 21:20:08 -0300

    Fixed an issue that prevented modifying a post when there is no reference on it

--- content/models.py
+++ content/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=256, help_text='The title of the post')
     author = models.ForeignKey(Login, on_delete=models.CASCADE, verbose_name='Created by', help_text='Account that created this entry. In case it comes from another social network, it will use any system account')
     content = models.TextField(null=True, blank=True, help_text='The body of the post')
-    reference = models.CharField(max_length=2048, help_text='Link to original post in case it comes from another social network')
+    reference = models.CharField(max_length=2048, null=True, default=None, help_text='Link to original post in case it comes from another social network')
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, default=None, verbose_name='In response to', help_text='Foreign key to another post in case it is a response')
 
     added = models.DateTimeField(auto_now_add=True, help_text='Date when it was created on RagCP')
