a9333668c5163c291c27e4e9f37efd1e8387f8a2
Giuliano P. Tognarelli Buono-core <gtognarelli@linux.com>
2020-10-11 15:01:30 -0300

    Fixed an issue with MySQL that prevented to fetch new content from RSS Feed because a default value does not works on Boolean fields

--- content/models.py
+++ content/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     content = models.TextField(null=True, blank=True, help_text='The body of the post')
     reference = models.CharField(max_length=2048, null=True, default=None, help_text='Link to original post in case it comes from another social network')
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, default=None, verbose_name='In response to', help_text='Foreign key to another post in case it is a response')
-    deleted = models.BooleanField(default=False)
+    deleted = models.BooleanField(default=False, null=True)
 
     added = models.DateTimeField(auto_now_add=True, help_text='Date when it was created on RagCP')
     updated = models.DateTimeField(auto_now=True, help_text='Date when modified')
