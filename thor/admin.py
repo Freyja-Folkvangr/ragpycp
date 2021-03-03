18d7bd8fa2e95773fb2bd7319610e32acaf746d4
Giuliano Tognarelli <gio@octocat.xyz>
2021-03-03 01:39:35 -0300

    Allow user to create patchers, add updates to the list and custom clients. (Does not stores update files)

--- thor/admin.py
+++ thor/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+from thor.models import Patcher, CustomClient, Update
+
+class PatcherAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'allow', 'force_start', 'policy_msg']
+    list_filter = ['allow', 'force_start']
+    search_fields = ['name', 'policy_msg']
+    list_display_links = ['name']
+    ordering = ['id']
+
+class CustomClientAdmin(admin.ModelAdmin):
+    list_display = ['patcher', 'name', 'checksum', 'path']
+    search_fields = ['name', 'path']
+    list_filter = ['patcher']
+    list_display_links = ['name']
+
+class UpdateAdmin(admin.ModelAdmin):
+    list_display = ['id', 'filename', 'patcher']
+    list_filter = ['patcher']
+    search_fields = ['filename']
+    list_display_links = ['filename']
+    ordering = ['-id', '-created']
+
+admin.site.register(Patcher, PatcherAdmin)
+admin.site.register(CustomClient, CustomClientAdmin)
+admin.site.register(Update, UpdateAdmin)
--- thor/views.py
+++ thor/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from thor.models import Patcher, CustomClient, Update
+
+
+def config(request, patcher_name):
+    patcher = get_object_or_404(Patcher, name=patcher_name)
+    clients = CustomClient.objects.filter(patcher=patcher).order_by('created')
+
+    # Set the custom client number
+    i = 1
+    for client in clients:
+        client.i = i
+        i += 1
+
+    context = {
+        'patcher': patcher,
+        'clients': clients
+    }
+    return render(request, 'main_config.html', context)
+
+def patch_list(request, patcher_name):
+    patches = Update.objects.filter(patcher__name=patcher_name)
+
+    context = {
+        'patches': patches
+    }
+    return render(request, 'patch_list.html', context)
